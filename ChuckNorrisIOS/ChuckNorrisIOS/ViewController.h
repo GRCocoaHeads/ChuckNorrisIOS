7d9e950e3589b6f9d4b8c4343837e7015825b77e
Ryan McCracken <ryan.mccracken@sagetech-llc.com>
2014-06-30 23:39:40 -0400

    Initial chuck splash page, welcome label, uzis image, nav in place

---
+++ ChuckNorrisIOS/ChuckNorrisIOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChuckNorrisIOS
+//
+//  Created by Ryan McCracken on 6/30/14.
+//  Copyright (c) 2014 Sagetech LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
