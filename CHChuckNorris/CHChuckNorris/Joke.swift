8c878ef6e0a7a51b2b1c1064d9b11cbcb10a7d15
Carlus Henry <carlushenry@gmail.com>
2014-09-12 12:19:48 -0400

    adding urls for chuck norris images

--- CHChuckNorris/CHChuckNorris/Joke.swift
+++ CHChuckNorris/CHChuckNorris/Joke.swift
@@ -11,11 +11,17 @@ import Foundation
 class Joke : NSObject {
     let id: Int
     let text: String
-    let imgUrl: NSURL = NSURL.URLWithString("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbBVpys8LV7PLRowgYcmkM9QKHSfh5BwCzK46UEuFAebnJYekB")
+    let imgUrl: NSURL 
+    let imageUrlStrings : [String] = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbBVpys8LV7PLRowgYcmkM9QKHSfh5BwCzK46UEuFAebnJYekB",
+        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTJ87pFYKnh0obZRvbCk0cJbDbr2B4O6T8egGJULWQBjaWOlYnIow",
+        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTg-AK_3jAC_idnPzukfrVtO2rEKk53VzblbC3_wbJ4CWERFm4V"
+    ]
     
     init(id: Int, text: String) {
         self.id = id
         self.text = text
+        self.imgUrl = NSURL.URLWithString(imageUrlStrings[1])
     }
     
     class func makeRequestToAPI(url: String, onCompletion:(NSDictionary) -> ()) {
